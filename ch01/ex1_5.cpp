ea241f9e2d67f068496127b45468e3ace1fbdc4d
Congyuan He <conhe@microsoft.com>
2017-11-14 20:14:41 +0800

    Initail upload

---
+++ ch01/ex1_5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+int main()
+{
+	int v1,v2;
+	std::cin>>v1>>v2;
+	std::cout<<"The product of ";
+	std::cout<<v1;
+	std::cout<<" and ";
+	std::cout<<v2;
+	std::cout<<" is ";
+	std::cout<<v1*v2;
+	std::cout<<std::endl;
+	return 0;
+}
