468ccbbfee656038d88f6be21be231fc19b90c4c
Congyuan He <conhe@microsoft.com>
2017-11-15 13:17:08 +0800

    update ch01

--- ch01/ex1_1.cpp
+++ ch01/ex1_1.cpp
@@ -1,3 +1,8 @@
+/*
+* Review the documentation for your compiler and determine 
+* what file naming convention it uses. Compiler and run the
+* main program from page 2
+*/
 int main()
 {
 	return 0;
