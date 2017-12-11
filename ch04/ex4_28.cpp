26398e292573826e97ce4ffc3837eca204ee81e4
Congyuan He <conhe@microsoft.com>
2017-12-11 23:59:50 +0800

    update ch04

---
+++ ch04/ex4_28.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"char: "<<sizeof(char)<<endl;
+	cout<<"short: "<<sizeof(short)<<endl;
+	cout<<"int: "<<sizeof(int)<<endl;
+	cout<<"long: "<<sizeof(float)<<endl;
+	cout<<"float: "<<sizeof(int)<<endl;
+	cout<<"double: "<<sizeof(double)<<endl;
+	return 0;
+}
