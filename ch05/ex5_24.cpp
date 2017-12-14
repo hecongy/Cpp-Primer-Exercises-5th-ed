9dd9a93ea2572848aab45835aa28c53c2eb7aa64
Congyuan He <conhe@microsoft.com>
2017-12-14 10:04:15 +0800

    update ch05

--- ch05/ex5_24.cpp
+++ ch05/ex5_24.cpp
@@ -4,6 +4,8 @@ int main()
 {
 	int i1, i2;
 	cin>>i1>>i2;	
+	if(i2==0)
+		throw runtime_error("Divider can't be 0.");
 	cout<<i1/i2<<endl;
 	return 0;
 }
--- ch05/ex5_25.cpp
+++ ch05/ex5_25.cpp
@@ -8,11 +8,13 @@ int main()
 	{
 		try
 	    {
+			if(i2==0)
+				throw runtime_error("Divider can't be 0.");
 	        cout<<i1/i2<<endl;
 	    }
 	    catch(exception err)
 	    {
-            cout<<err.what()<<"\n Try again? Enter y or n"<<endl;
+            cout<<err.what()<<"\nTry again? Enter y or n"<<endl;
             char c;
 			cin>>c;
 			if(!cin || c=='n')
